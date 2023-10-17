0e733720b5c065dbe0ed6413ee6f06a5ac1e09e5
GenesisCorrea <gen.correa@duocuc.cl>
2023-10-16 22:48:23 -0300

    appcambios

---
+++ src/app/guards/no-auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FirebaseService } from '../service/firebase.service';
+import { UtilsService } from '../service/utils.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoAuthGuard implements CanActivate {
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    
+    
+      return new Promise(( resolve ) =>{
+
+        this.firebaseSvc.getAuth().onAuthStateChanged((auth) => {
+
+          if(!auth)resolve(true);
+          else{
+            this.utilsSvc.routerLink('/main/home');
+            resolve(false);
+          }
+        })
+         
+      });
+  }
+  
+}
+  
